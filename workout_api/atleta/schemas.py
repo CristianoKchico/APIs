59b7604ee9a69fdf55d20c488d90e5caba3f907b
CristianoKchico <cristianofdsantos@gmail.com>
2025-01-17 12:28:09 -0300

    Atualizações da aula "Inserindo configurações do banco de dados e adicionando settings"

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -1,5 +1,7 @@
 from typing import Annotated
-from pydantic import BaseSchema, Field, PositiveFloat
+from pydantic import Field, PositiveFloat
+
+from workout_api.contrib.schemas import BaseSchema, OutMixin
 
 class Atleta(BaseSchema):
     nome: Annotated[str, Field(description="nome do atleta", example="João", max_length=50)]
@@ -8,4 +10,9 @@ class Atleta(BaseSchema):
     peso: Annotated[PositiveFloat, Field(description="peso do atleta", example="56.5")]
     altura: Annotated[PositiveFloat, Field(description="altura do atleta", example="1.75")]
     sexo: Annotated[str, Field(description="sexo do atleta", example="M", max_length=1)]
-    
+
+class AtletaIn(Atleta):
+    pass
+
+class AtletaOut(Atleta, OutMixin):
+    pass
